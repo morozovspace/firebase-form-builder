14533a91018a50df62c3bd9778b3a659f753c1a1
Nikita Morozov <morozov.space@mail.ru>
2020-12-29 22:35:25 +0300

    delete console log

--- services/frontend/app/src/store/auth/mutations.js
+++ services/frontend/app/src/store/auth/mutations.js
@@ -2,6 +2,5 @@ export default {
   SET_AUTH_USER: (state, { authUser, claims }) => {
     const { uid, email, emailVerified } = authUser
     state.user = { uid, email, emailVerified }
-    console.log(claims)
   },
 }
