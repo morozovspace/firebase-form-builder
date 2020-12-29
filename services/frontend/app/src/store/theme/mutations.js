9402feb4ee3e9b66a9717b16eac8ec4f44783a53
Nikita Morozov <morozov.space@mail.ru>
2020-12-29 22:35:04 +0300

    Change cookies path

--- services/frontend/app/src/store/theme/mutations.js
+++ services/frontend/app/src/store/theme/mutations.js
@@ -1,6 +1,8 @@
 export default {
   SET_THEME(state, value) {
     state.theme = value
-    this.$cookies.set("theme", state.theme, { path: process.env.GITHUB_REPOSITORY })
+    this.$cookies.set("theme", state.theme, {
+      path: process.env.GITHUB_REPOSITORY,
+    })
   },
 }
