aa4c5fdae51247e523e9844dd323f38c25a9e9c7
Nikita Morozov <morozov.space@mail.ru>
2021-01-01 19:19:00 +0300

    Focus-visible support

--- services/frontend/app/config/head.js
+++ services/frontend/app/config/head.js
@@ -1,5 +1,6 @@
+const path = require('path')
 export default { 
-  title: process.env.PWA_OG_TITLE,
+  title: process.env.PWA_OG_TITLE || "PWA_OG_TITLE",
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -11,6 +12,11 @@ export default {
       name: 'theme-color',
       content: '#2b2b2b'
     },
+  ],
+  script: [
+    {
+      src: "https://unpkg.com/focus-visible"
+    }
   ],
   link: [
     { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Heebo:100,300,400,500,700,800,900" },
--- services/frontend/app/config/styles.js
+++ services/frontend/app/config/styles.js
@@ -2,9 +2,8 @@ export default {
   scss: [
     "@/assets/styles/default/_reset.scss",
     "@/assets/styles/default/_variables.scss",
-    "@/assets/styles/default/_fonts.scss",
+    "@/assets/styles/default/_focus-visible.scss",
     "@/assets/styles/default/_mixins.scss",
-    "@/assets/styles/default/_animation.scss",
     "@/assets/styles/default/_themes.scss",
   ],
 }
